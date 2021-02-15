5456d2955cd67890c1fd6fdb6917822aaae763d9
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2021-02-15 01:04:18 +0100

    add redirect functionality

---
+++ src/hooks/use-content.js
@@ -0,0 +1,26 @@
+import { useState, useEffect, useContext } from "react";
+import { firebase, FirebaseContext } from "../context/firebase";
+
+export default function useContent(target) {
+    const [content, setContent] = useState([]);
+    const context = useContext(FirebaseContext);
+
+    useEffect(() => {
+        firebase
+            .firestore()
+            .collection(target)
+            .get()
+            .then((snapshot) => {
+                const allContent = snapshot.docs.map((contentObj) => ({
+                    ...contentObj.data(),
+                    docId: contentObj.id,
+                }));
+                setContent(allContent);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }, []);
+
+    return {[target]: content}
+}
