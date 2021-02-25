427a937d1dd4ca47683aa7c87bda15940231830c
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2021-02-25 11:45:36 +0100

    app finished, testing next

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowseContainer } from "../containers/browse";
 import { useContent } from "../hooks";
-import selectionFilter from "../utils/selection-filter";
+import {selectionFilter} from "../utils/selection-filter";
 
 export default function Browse() {
     const { series } = useContent("series");
